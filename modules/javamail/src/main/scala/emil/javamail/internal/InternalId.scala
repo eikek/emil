9617441aa7ac3066b65add9790a013f34c6e40c9
Eike Kettner <eike.kettner@posteo.de>
2020-05-25 10:36:18 +0200

    Fix reading internal id

--- modules/javamail/src/main/scala/emil/javamail/internal/InternalId.scala
+++ modules/javamail/src/main/scala/emil/javamail/internal/InternalId.scala
@@ -2,6 +2,7 @@ package emil.javamail.internal
 
 import cats.implicits._
 import com.sun.mail.imap.IMAPFolder
+import org.log4s.getLogger
 
 sealed trait InternalId {
 
@@ -10,6 +11,7 @@ sealed trait InternalId {
 }
 
 object InternalId {
+  private[this] val logger = getLogger
 
   case class MessageId(id: String) extends InternalId {
     val asString = s"messageId:$id"
@@ -24,19 +26,23 @@ object InternalId {
   def makeInternalId(msg: SafeMimeMessage): InternalId =
     msg.getFolder match {
       case Some(imf: IMAPFolder) =>
-        Util.withReadFolder(imf)(_ => InternalId.Uid(imf.getUID(msg.delegate)))
+        Util.withReadFolder(imf)(_ => Uid(imf.getUID(msg.delegate)))
       case _ =>
-        InternalId.MessageId(
-          msg.getMessageID.getOrElse(sys.error("No message id present"))
-        )
+        msg.getMessageID match {
+          case Some(mid) =>
+            MessageId(mid)
+          case _ =>
+            logger.info(s"Mail '${msg.getSubject}' has no message id")
+            NoId
+        }
     }
 
   def readInternalId(str: String): Either[String, InternalId] =
     str.split(':').toList match {
       case p :: id :: Nil =>
         if (p == "uid")
-          Either.catchNonFatal(InternalId.Uid(id.toLong)).leftMap(_.getMessage)
-        else if (p == "messageId") Either.right(InternalId.MessageId(id))
+          Either.catchNonFatal(Uid(id.toLong)).leftMap(_.getMessage)
+        else if (p == "messageId") Either.right(MessageId(id))
         else Right(NoId)
       case _ =>
         Either.left(s"Invalid id: $str")
