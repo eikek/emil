a02ead0b430ef916188e4ef1c4e67be505aeed33
Scala Steward <me@scala-steward.org>
2020-08-16 18:15:02 +0200

    Update greenmail to 1.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val doobieVersion = "0.9.0"
   val flexmarkVersion = "0.62.2"
   val fs2Version = "2.4.2"
-  val greenmailVersion = "1.5.14"
+  val greenmailVersion = "1.6.0"
   val h2Version = "1.4.200"
   val javaxMailVersion = "1.6.5"
   val jsoupVersion = "1.13.1"
