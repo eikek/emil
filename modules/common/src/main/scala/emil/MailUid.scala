251c12a323f4264172a315b5d5d6ee32ca9e2e1c
Ondra Pelech <ondra.pelech@gmail.com>
2022-01-22 16:20:03 +0100

    Add functionality to work with UID

--- modules/common/src/main/scala/emil/MailUid.scala
+++ modules/common/src/main/scala/emil/MailUid.scala
@@ -6,11 +6,15 @@ final case class MailUid(n: Long) {
   override def toString = s"uid:$n"
   def next: MailUid     = MailUid(n + 1)
 }
-object MailUid {
+
+object MailUid extends MailUidLowPriorityImplicits {
   def lastUid: MailUid = MailUid(-1)
   def maxUid: MailUid  = MailUid(0xffffffffL)
 
-  implicit lazy val hash: Hash[MailUid]   = Hash.fromUniversalHashCode[MailUid]
+  implicit lazy val hash: Hash[MailUid] = Hash.fromUniversalHashCode[MailUid]
+  implicit lazy val show: Show[MailUid] = Show.fromToString[MailUid]
+}
+
+trait MailUidLowPriorityImplicits {
   implicit lazy val order: Order[MailUid] = Order.by[MailUid, Long](_.n)
-  implicit lazy val show: Show[MailUid]   = Show.fromToString[MailUid]
 }
