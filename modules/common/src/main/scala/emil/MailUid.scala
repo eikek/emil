fe583d6e889c519a6d0e8d923ce6bc2df646d9a2
Ondra Pelech <ondra.pelech@gmail.com>
2022-01-24 14:36:06 +0100

    fix

--- modules/common/src/main/scala/emil/MailUid.scala
+++ modules/common/src/main/scala/emil/MailUid.scala
@@ -4,12 +4,12 @@ import cats.{Hash, Order, Show}
 
 final case class MailUid(n: Long) {
   override def toString = s"uid:$n"
-  def next: MailUid     = MailUid(n + 1)
+  def next: MailUid = MailUid(n + 1)
 }
 
 object MailUid extends MailUidLowPriorityImplicits {
   def lastUid: MailUid = MailUid(-1)
-  def maxUid: MailUid  = MailUid(0xffffffffL)
+  def maxUid: MailUid = MailUid(0xffffffffL)
 
   implicit lazy val hash: Hash[MailUid] = Hash.fromUniversalHashCode[MailUid]
   implicit lazy val show: Show[MailUid] = Show.fromToString[MailUid]
