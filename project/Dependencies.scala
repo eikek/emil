ca202c69cdb34f371e487a4fc2814d736273ae4f
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-06-15 06:14:12 +0000

    Update h2 to 2.1.214

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val flexmarkVersion = "0.62.2"
   val fs2Version = "3.2.7"
   val greenmailVersion = "2.0.0-alpha-2"
-  val h2Version = "2.1.212"
+  val h2Version = "2.1.214"
   val munitVersion = "0.7.29"
   val javaxMailVersion = "2.0.1"
   val jsoupVersion = "1.15.1"
