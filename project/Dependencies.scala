64a609acc5833ee4d4af7216c794ca5e0505aea4
Scala Steward <scala_steward@virtuslab.com>
2023-05-03 14:34:13 +0000

    Update flexmark, ... to 0.64.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val doobieVersion = "1.0.0-RC2"
-  val flexmarkVersion = "0.62.2"
+  val flexmarkVersion = "0.64.4"
   val fs2Version = "3.6.1"
   val greenmailVersion = "2.0.0"
   val h2Version = "2.1.214"
