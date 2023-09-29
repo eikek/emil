dcaed88e1ee0b1ba1f9265ec23f1b1acb371cbe2
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2023-09-29 06:27:07 +0000

    Update poi, poi-scratchpad to 5.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val log4sVersion = "1.10.0"
   val logbackVersion = "1.2.12"
   val miniTestVersion = "2.9.6"
-  val poiVersion = "5.2.3"
+  val poiVersion = "5.2.4"
 
   val munit = Seq(
     "org.scalameta" %% "munit" % munitVersion,
