731712341cef94b16095639c271d38818920dcf4
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-09-11 06:08:18 +0000

    Update fs2-core, fs2-io to 3.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val doobieVersion = "1.0.0-RC5"
   val flexmarkVersion = "0.64.8"
-  val fs2Version = "3.10.2"
+  val fs2Version = "3.11.0"
   val greenmailVersion = "2.0.1"
   val h2Version = "2.3.230"
   val munitVersion = "0.7.29"
