a3751ea4841bdab732b3842bd62155c0d28ac840
Scala Steward <scala_steward@virtuslab.com>
2024-09-18 15:33:34 +0000

    Update doobie-core to 1.0.0-RC6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val doobieVersion = "1.0.0-RC5"
+  val doobieVersion = "1.0.0-RC6"
   val flexmarkVersion = "0.64.8"
   val fs2Version = "3.10.2"
   val greenmailVersion = "2.0.1"
