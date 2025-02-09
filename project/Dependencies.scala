f0b94b66e595b573881f15d917a69fc0038d6818
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-02-09 06:12:43 +0000

    Update doobie-core to 1.0.0-RC7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val doobieVersion = "1.0.0-RC6"
+  val doobieVersion = "1.0.0-RC7"
   val flexmarkVersion = "0.64.8"
   val fs2Version = "3.11.0"
   val greenmailVersion = "2.0.1"
